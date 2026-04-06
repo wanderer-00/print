13bedbe257e8646599b17a4c95126dbba8baffcc
wanderer <56039464+wanderer-00@users.noreply.github.com>
2026-04-06 19:10:51 +0300

    -

---
+++ script/dizziness.js
@@ -0,0 +1,23 @@
+const display = document.getElementById('display');
+
+
+
+display.innerHTML = `
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+`
+
+display.innerHTML += `
+    <div class="a"></div>
+    <div class="a"></div>
+    <div class="a"></div>
+    <div class="a"></div>
+    <div class="a"></div>
+`
