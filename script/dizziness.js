cb2188d605cfca9f3397d1179a8e377e29d72e53
wanderer <56039464+wanderer-00@users.noreply.github.com>
2026-04-08 04:14:30 +0300

    -

--- script/dizziness.js
+++ script/dizziness.js
@@ -1,23 +1,27 @@
 const display = document.getElementById('display');
+const tasks = document.getElementById('task');
 
+// обработка JSON
+async function loadData() {
+    try {
+        const response = await fetch('../data/dizziness.json'); // путь к файлу
+        const data = await response.json(); // парсим JSON в объект JS
+        console.log(data); // работаем с данными
 
+        // заполнение заказов
+        for (i = 0; i < data.length; i++) {
+            tasks.innerHTML += `
+                <div>${data[i][0]}</div>
+            `;
+            for (n = 0; n < data[i][1]; n++) {
+                display.innerHTML += `<div class="a${i}"></div>`
+            }
+        }
+        tasks.innerHTML += '<div>Ваш заказ</div>'
 
-display.innerHTML = `
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-`;
+    } catch (error) {
+        console.error('Ошибка загрузки:', error);
+    }
+}
 
-display.innerHTML += `
-    <div class="a"></div>
-    <div class="a"></div>
-    <div class="a"></div>
-    <div class="a"></div>
-    <div class="a"></div>
-`;
+loadData();
