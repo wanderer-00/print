e990bcdded096dbee2ce8a364ae1a511c1e19f7b
wanderer <56039464+wanderer-00@users.noreply.github.com>
2026-04-09 17:12:20 +0300

    -

--- script/calc.js
+++ script/calc.js
@@ -1,9 +1,18 @@
 const count = document.getElementById('count');
 const price = document.getElementById('price');
 
-price.innerHTML = ` &#8381;`;
+price.innerHTML = `100,00 &#8381;`;
 
 count.addEventListener('input', (event) => {
+    const summ = event.target.value * 100;
+    
+    // форматирование валюты
+    const formatter = new Intl.NumberFormat('ru-RU', {
+      style: 'currency',
+      currency: 'RUB',
+      minimumFractionDigits: 2, // копейки
+    });
+
   // Получаем текущее значение через event.target.value
-  price.innerHTML = `${event.target.value * 100} &#8381;`;
+  price.innerHTML = formatter.format(summ);
 });
